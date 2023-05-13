388c443b87a0054963deac4250fb668b5009e907
ImDinnerCat <mail@martin-herz.io>
2023-05-13 06:10:32 +0200

    [add] Textbox component

---
+++ src/components/utilities/Textbox.tsx
@@ -0,0 +1,33 @@
+// React imports
+import React, { useEffect, useState } from 'react'
+
+// Properties: Textbox
+export type Props = {
+    value?: string;
+    placeholder?: string;
+    type?: string;
+    onValueChange?: (val: string) => void;
+}
+
+// Component: Textbox
+export const Textbox: React.FC<Props> = (
+    {
+        value = "",
+        placeholder = "",
+        type = "text",
+        onValueChange = () => {}
+    }
+) => {
+
+    // Value state
+    const [inputValue, setInputValue] = useState(value)
+
+    // Update value state
+    useEffect(() => {
+        onValueChange(inputValue)
+        console.log(inputValue)
+    }, [inputValue, onValueChange])
+
+    
+    return <input type={type} placeholder={placeholder} className={"textbox"} value={inputValue} onChange={e => setInputValue(e.target.value)} />
+}
