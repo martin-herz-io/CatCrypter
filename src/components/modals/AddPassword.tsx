7549bbfbe6c5694d4ff8f7b55530fae65372eec1
ImDinnerCat <mail@martin-herz.io>
2023-05-15 10:25:09 +0200

    [implement] Encryption & file save

--- src/components/modals/AddPassword.tsx
+++ src/components/modals/AddPassword.tsx
@@ -1,11 +1,16 @@
 // React imports
 import React, { useState } from 'react'
+import { fs } from '@tauri-apps/api'
+import * as CryptoJS from 'crypto-js'
 import { Textbox } from '../utilities/Textbox';
 
 // Properties: Textbox
 export type Props = {
     passwordList: { title: string; username: string; password: string; }[];
     setPasswordList: React.Dispatch<React.SetStateAction<{ title: string; username: string; password: string; }[]>>;
+    trayList: { title: string; logo?: string; color?: string; date: string; location: string; }[];
+    selectedTray: number|null;
+    currentTrayPassword: string;
     setModalState: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
@@ -14,6 +19,9 @@ export const AddPassword: React.FC<Props> = (
     {
         passwordList,
         setPasswordList,
+        trayList,
+        selectedTray,
+        currentTrayPassword,
         setModalState
     }
 ) => {
@@ -25,11 +33,54 @@ export const AddPassword: React.FC<Props> = (
 
     // Add new password to list
     const addPassword = () => {
+
+        // Add new password to list
         setPasswordList([...passwordList, {
             "title": editTitleInput,
             "username": editUsernameInput,
             "password": editPasswordInput,
         }])
+
+        // Save password list to file
+        const trayPath = trayList[selectedTray!].location
+        fs.readTextFile(trayPath).then((data) => {
+            const trayFile = JSON.parse(data)
+            
+            // Check if accounts list exists
+            let decryptedData
+            if (trayFile.accounts === null) {
+
+                // Create accounts list
+                decryptedData = []
+            } else {
+
+                // Decrypt password list
+                const bytes = CryptoJS.AES.decrypt(trayFile.accounts, currentTrayPassword)
+                decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
+            }
+            
+            // Add new password to list
+            decryptedData.push({
+                "title": editTitleInput,
+                "username": editUsernameInput,
+                "password": editPasswordInput,
+            })
+
+            // Encrypt password list
+            const encryptedData = CryptoJS.AES.encrypt(JSON.stringify(decryptedData), currentTrayPassword).toString()
+
+            // Save password list to file
+            fs.writeTextFile(trayPath, JSON.stringify({
+                "title": trayFile.title,
+                "logo": trayFile.logo,
+                "color": trayFile.color,
+                "date": trayFile.date,
+                "password": trayFile.password,
+                "accounts": encryptedData
+            }))
+        })
+
+        // Close modal
         setModalState(false)
     }
 
