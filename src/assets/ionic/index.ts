1c1e9794c060ad639b2f59facb680ca68bdfd1da
ImDinnerCat <mail@martin-herz.io>
2023-05-15 11:06:17 +0200

    [remove] index

--- src/assets/ionic/index.ts
+++
@@ -1,4 +0,0 @@
-// Exports
-export { get_ionic_line_icon } from './get_ionic_line_icons';
-export { get_ionic_sharp_icon } from './get_ionic_sharp_icons';
-export { get_ionic_solid_icon } from './get_ionic_solid_icons';
