1db0c1cb1ecea66fca6e2ef4a568619160a725ad
ImDinnerCat <mail@martin-herz.io>
2023-09-21 15:15:44 +0200

    [add] language: pt

--- src/scripts/i18n.ts
+++ src/scripts/i18n.ts
@@ -4,12 +4,14 @@ import { initReactI18next } from 'react-i18next';
 // Localizations
 import en from '../assets/locales/en.json';
 import de from '../assets/locales/de.json';
+import pt from '../assets/locales/pt.json';
 // Localizations
 
 i18n.use(initReactI18next).init({
     resources: {
         en: { translation: en },
-        de: { translation: de }
+        de: { translation: de },
+        pt: { translation: pt }
     },
     fallbackLng: "en",
     interpolation: { escapeValue: false }
