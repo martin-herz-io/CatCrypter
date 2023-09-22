250036c94e8252efb7f593bd6c47ced714902a16
ImDinnerCat <mail@martin-herz.io>
2023-09-22 12:02:23 +0200

    [add] language dk, fr & nl

--- src/scripts/i18n.ts
+++ src/scripts/i18n.ts
@@ -2,16 +2,24 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 // Localizations
-import en from '../assets/locales/en.json';
-import de from '../assets/locales/de.json';
-import pt from '../assets/locales/pt.json';
+import de from '../assets/locales/de.json'; // German
+import dk from '../assets/locales/dk.json'; // Danish
+import en from '../assets/locales/en.json'; // English
+import es from '../assets/locales/es.json'; // Spanish
+import fr from '../assets/locales/fr.json'; // French
+import nl from '../assets/locales/nl.json'; // Dutch
+import pt from '../assets/locales/pt.json'; // Portuguese
 // Localizations
 
 i18n.use(initReactI18next).init({
     resources: {
-        en: { translation: en },
-        de: { translation: de },
-        pt: { translation: pt }
+        de: { translation: de }, // German
+        dk: { translation: dk }, // Danish
+        en: { translation: en }, // English
+        es: { translation: es }, // Spanish
+        fr: { translation: fr }, // French
+        nl: { translation: nl }, // Dutch
+        pt: { translation: pt } // Portuguese
     },
     fallbackLng: "en",
     interpolation: { escapeValue: false }
